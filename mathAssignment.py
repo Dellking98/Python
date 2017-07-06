49dc6e40c763fb5060fccaa50fe1eb3adebafa1e
Dellydell19git config --global user.name Dellydell19git config --global user.name Dellydell19 <carolyntorain@yahoo.com>
2017-07-05 21:50:08 -0500

    Odd numbers, multiples, Sum ,Average

---
+++ mathAssignment.py
@@ -0,0 +1,31 @@
+# Multiples
+    # printing odd numbers
+# for count in range (1,1000):
+#     if count % 2 != 0:
+#         print count
+
+    # printing multiples of 5
+# for num in range (5, 1000000):
+#     if num % 5 ==0:
+#         print num
+
+# Sum
+    #printing the sum of the lise
+
+# a = [1, 2, 5, 10, 255, 3]
+#
+# num = sum(a)
+#
+# print num
+
+# Average
+
+    #printing Average in list
+
+a = [1, 2, 5, 10, 255, 3]
+
+num = sum(a)
+
+avg = num / len(a)
+
+print avg
