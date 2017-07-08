f5db81d4ad4d768b27e8076f58407981c1124a78
Dellking98 <carolyntorain@yahoo.com>
2017-07-07 21:29:45 -0500

    Add files via upload

---
+++ Find_characters.py
@@ -0,0 +1,17 @@
+
+word_list = ['hello','world','my','name','is','Anna']
+char = 'i'
+
+new_arr = []
+
+for i in word_list:
+    if i.find(char) != -1:
+        new_arr.append(i)
+
+print new_arr
+
+
+
+
+
+#results = word_list.split(',')
