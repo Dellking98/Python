f164906e22cc4388156695caaed5acafe04be563
Dellking98 <carolyntorain@yahoo.com>
2017-07-07 23:00:07 -0500

    Fixing as I go

--- mixType.py
+++ mixType.py
@@ -1,21 +1,38 @@
 
 l = ['magical unicorns',19,'hello',98.89,'world']
-string = []
-sum1 = []
+lA = [2,3,1,7,4,12]
+sA = ["car", 'DoJO', 'sleeping']
 
-for types in l:
-    testing = type(types)
+sum = 0;
+new_string = ""
 
+testing = lA
+new_arr = ""
 
-    if testing is int:
-        sum1.append(types)
-        num1 = sum(sum1)
+for i in range(0,len(testing)):
 
-    elif testing is str:
-        string.append(types)
+    if isinstance(testing, int) == int:
+        new_arr = "int"
 
+    elif isinstance(testing, str) == str:
+        new_arr = "str"
 
+    else:
+        new_arr = 'mixed'
 
-print testing
-print " String: {}".format(string)
-print "Sum:",num1
+for x in range(0,len(tesing)):
+
+    if type(l) is int:
+        sum1 = sum + testing[x]
+
+    elif type(l) is str:
+        new_string.append(x)
+
+if new_arr == "int":
+    print "Sum:", sum1
+elif new_arr == "str":
+    print "String :", new_string
+elif new_arr == 'mixed':
+    print "This is a Mixed List"
+    print "String :", new_string
+    print "Sum:", sum1
